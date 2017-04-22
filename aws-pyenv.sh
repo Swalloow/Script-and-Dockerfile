9d12e0fdd7014b8635e4325ad867fee752383071
Swalloow <joon920570@gmail.com>
2017-04-22 16:18:33 +0900

    Add aws pyenv setting scripts

---
+++ aws-pyenv.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+xz-utils tk-dev
+
+curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+
+# pyenv settings
+echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+source ~/.bashrc
+
+pyenv update
