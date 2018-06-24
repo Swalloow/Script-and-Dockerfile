fc7639dc34944eb87aec1a47ffa5a94891d466b1
Swalloow <swalloow.me@gmail.com>
2018-06-24 20:14:04 +0900

    Add macbook settings

---
+++ macbook.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# https://subicura.com/2017/11/22/mac-os-development-environment-setup.html
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install git
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+brew install zsh-completions
+chsh -s `which zsh`
+
+# powerlevel9k theme
+# add ZSH_THEME="powerlevel9k/powerlevel9k"
+# change powerline fonts in iTerm2
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts && ./install.sh
+cd ..
+rm -rf fonts
+
+# zsh-syntax-highlighting zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+# add into ~/.zshrc file
+# plugins=(
+#   git
+#   zsh-syntax-highlighting
+#   zsh-autosuggestions
+# )
+
+brew install fzf fasd tree
+$(brew --prefix)/opt/fzf/install
+
+# install pyenv
+brew install pyenv pyenv-virtualenv
+echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshrc
+source ~/.zshrc
